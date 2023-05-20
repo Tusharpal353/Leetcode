ba138a2c37641fb1b4368ad9f5468478042b7ffe
Tushar Pal <Tusharpal353@gmail.com>
2023-05-20 23:08:13 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -13,18 +13,43 @@ app.use(bodyParser.urlencoded({ extended: true })); // write this code everytime
 //   res.sendFile(__dirname + "/signup.html");
 // });
 
+
+//***************************SignUP**************************************
+const users=[]
+
 app.get("/signup", function (req, res) {
   // res.send("hello")
   res.sendFile(__dirname + "/signup.html");
 });
 app.post('/signup', (req, res) => {
-  // Your code logic here
+  const { username, email } = req.body;
+  const pass = req.body.password;
 
+  const newUser = {
+    email,
+    pass
+  };
+
+  // Push the new user object to the "users" array
+  users.push(newUser);
   // Send a 200 OK status code
   res.status(200).send('Success');
 });
+//***************************SignUP**************************************
+//****************************questions ********************************* */
+const QUESTIONS = [{
+  title: "Two states",
+  description: "Given an array , return the maximum of the array?",
+  testCases: [{
+      input: "[1,2,3,4,5]",
+      output: "5"
+  }]
+}];
+app.post("/questions",function(req,res){
+  res.send(QUESTIONS)
+})
 
-
+//****************************questions ********************************* */
 
 app.listen(3000, function () {
   console.log("server is started on port 3000");
